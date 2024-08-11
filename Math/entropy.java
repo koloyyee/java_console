35a41a5f4466835c835f563ae4fff1f867fe124a
Loy Yee Ko <koloyyee@gmail.com>
2024-08-11 10:59:03 -0400

    entropy

--- Math/entropy.java
+++ Math/entropy.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-import static java.lang.System.*;
+import java.util.Arrays;
 
 public class entropy {
 
@@ -11,6 +11,19 @@ public class entropy {
     for (int i = 0; i < p.length; i++) {
       ans += -(p[i] * (Math.log(p[i]) / Math.log(2)));
     }
-    System.out.println(ans);
+    System.out.println("PP: " +  ans);
+
+    entropy(args);
+  }
+
+  public static double entropy(String... args) {
+    if (args.length <= 1)
+      return 0;
+    double sum = Arrays.stream(args).mapToDouble(Double::parseDouble)
+        .map(el -> -(el * (Math.log(el) / Math.log(2))))
+        .sum();
+        System.out.println("FP: " + sum);
+    return sum;
+
   }
 }
